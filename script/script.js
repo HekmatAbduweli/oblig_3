9b7b52cd536eb2aabfab2575869812fe9db33415
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2023-10-23 16:08:17 +0200

    ...

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// const bar = document.getElementById("bar");
+//       let navBar = document.getElementById("navbar");
+
+//       bar.addEventListener("click", (event) => {
+//         if (navBar.classList.contains("navbar")) {
+//           navBar.classList.remove("navbar");
+//           navBar.classList.add("responsive");
+//         } else {
+//           navBar.classList.add("navbar");
+//           navBar.classList.remove("responsive");
+//         }
+//       });
+
